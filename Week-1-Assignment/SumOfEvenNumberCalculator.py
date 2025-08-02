a78a7e03600217a6073eed8c02fda01bfc957278
DipakNeupane1 <dipakaanupam@gmail.com>
2025-08-03 09:05:34 +1200

    calculate sum of odd numbers as well and print all even numbers

--- Week-1-Assignment/SumOfEvenNumberCalculator.py
+++ Week-1-Assignment/SumOfEvenNumberCalculator.py
@@ -1,13 +1,19 @@
 def calculateSum(number):
     sum=0;
+    sumOfOdd=0;
     if number<=0:
         return print("Given number is 0 or negative, so please enter positive number for sum calculation.")
     else:
         i=1;
         while i <=number:
          if i % 2 == 0:
+           print("Even number is ::", i)
            sum+=i
+         else:
+            print("Odd number is :: ", i)
+            sumOfOdd+=i
          i+=1
+    print(f"Sum of odd numbers between 1 to {number} is :: ", sumOfOdd)
     return sum
 
 if __name__ == "__main__":
