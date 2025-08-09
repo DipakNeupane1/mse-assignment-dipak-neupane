a9ec4e3f66516935a224678cfbb8e2b467b42211
DipakNeupane1 <dipakaanupam@gmail.com>
2025-08-10 08:46:42 +1200

    Class constructor is removed and make the code workable through instance methods.

--- Week-2-Assignment/StringManipulator.py
+++ Week-2-Assignment/StringManipulator.py
@@ -1,20 +1,18 @@
 class StringManipulator:
-    def __init__(self, text):
-        self.text = text
-        
-    def find_character(self, char):
-        return self.text.find(char)
+    # These are instance methods of the class
+    def find_character(self, input_string, char):
+        return input_string.find(char)
     
-    def find_length(self):
-        return len(self.text) 
+    def find_length(self, input_string):
+        return len(input_string) 
     
-    def to_uppercase(self):
-        return self.text.upper()
+    def to_uppercase(self, input_string):
+        return input_string.upper()
     
-obj_string_manipulator = StringManipulator("test")
-char_val=obj_string_manipulator.find_character('e')
+obj_string_manipulator = StringManipulator()
+char_val=obj_string_manipulator.find_character("Testing",'e')
 print(f"Index of a char is :: {char_val}")
-length_of_val=obj_string_manipulator.find_length()
-upper_case_value=obj_string_manipulator.to_uppercase()
+length_of_val=obj_string_manipulator.find_length("Testing")
+upper_case_value=obj_string_manipulator.to_uppercase("testing")
 print(f"Length of a string is :: {length_of_val}")
 print(f"Uppercase value of a string is ::",upper_case_value)
