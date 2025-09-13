e9e343022e4021e08b25c105978bdbec4c83b354
DipakNeupane1 <dipakaanupam@gmail.com>
2025-09-13 12:08:51 +1200

    Add singleton db connection pool

--- Week-7-Assignment/user_service.py
+++ Week-7-Assignment/user_service.py
@@ -1,13 +1,13 @@
 #Week 7 - Activity 1 (part1) : Design Pattern - Tips for Python exercises
 #Develop a code, record the processing time, 
 # and share the results. Provide the GitHub link for reference. Hint: You may adjust the queries to align with your database structure. - See below file
-import sqlite3
 from datetime import datetime
+from db_connection import DatabaseConnection
 class BookService:
    def  get_book(self, book_id):
        print(f"Get book =======>>")
        print(f"Requested on :: {datetime.now()}")
-       db_conn = sqlite3.connect('library.db')
+       db_conn = DatabaseConnection().get_connection()
        cursor = db_conn.cursor()
        cursor.execute('Select * from books where id = ?',(book_id,))
        row = cursor.fetchone()
@@ -20,7 +20,7 @@ class BookService:
    def get_books(self):
         print(f"Get books =======")
         print(f"Requested time is :: {datetime.now()}")
-        db_conn = sqlite3.connect('library.db')
+        db_conn = DatabaseConnection().get_connection()
         cursor = db_conn.cursor()
         cursor.execute('Select * from books where 1=1')
         row = cursor.fetchone()
