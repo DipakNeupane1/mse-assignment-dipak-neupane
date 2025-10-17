58267e49ee370a96470c9d320f141b7023a13090
DipakNeupane1 <dipakaanupam@gmail.com>
2025-10-18 11:35:44 +1300

    new api added.

--- Week-7-Assignment/Week-10-Assignment/app.py
+++ Week-7-Assignment/Week-10-Assignment/app.py
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def hello_flask():
     return "<p>Hello, Flask!</p>"
 
+
+#Can you use the following code and analyze it? 
+ 
+#Sample code:
+@app.route('/cal/<int:num>')
+def show_square(num):
+    return f"The square of {num} is {num**2}"
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5001, debug=True)
