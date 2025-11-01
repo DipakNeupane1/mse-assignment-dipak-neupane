fe25fc9fdf3b9334efc73af0e0889e46966c8f74
DipakNeupane1 <dipakaanupam@gmail.com>
2025-11-02 10:15:13 +1300

    count events from each category.

--- Week-14/web_scrapping.py
+++ Week-14/web_scrapping.py
@@ -10,3 +10,21 @@ soup = BeautifulSoup(response.text, 'html.parser')
 """Find the number of upcoming events"""
 event_count = soup.find('span', class_='badge bg-primary').text
 print(f"The number of upcoming events are : {event_count}")
+
+# ---- Retrieve event counts per category with each links ----
+categories = {
+    "Māori": "/events/tag/m%C4%81ori",
+    "Pacific": "/events/tag/pacific",
+    "General": "/events/tag/general",
+}
+
+for category, path in categories.items():
+    category_url = f"{url}{path}"
+    category_response = requests.get(category_url)
+    category_soup = BeautifulSoup(category_response.text, 'html.parser')
+
+    # Find all the event cards from each event is within a <div class="card">
+    events = category_soup.find_all("div", class_="card")
+    event_count = len(events)
+
+    print(f"{category} events: {event_count}")
